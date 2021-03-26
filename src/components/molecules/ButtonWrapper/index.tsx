b2c81313c907b1779304fa3d7bdbd5aa86cc8ce6
untlsn <untellson@gmail.com>
2021-03-26 19:34:19 +0100

    create ButtonWrapper

--- src/components/molecules/ButtonWrapper/index.tsx
+++ src/components/molecules/ButtonWrapper/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import * as S from './style';
+import IconButton from '@atoms/IconButton';
+import * as I from '@assets/icons/fileIcons';
 
 const ButtonWrapper = () => {
   return (
-    <S.Wrapper />
+    <S.Wrapper>
+      <IconButton iconSrc={I.iconDocument} />
+      <IconButton iconSrc={I.iconFolder} />
+      <IconButton iconSrc={I.iconUpload} />
+    </S.Wrapper>
   );
 };
 
